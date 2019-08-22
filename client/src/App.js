78c10c9e8a4b78d37cee0f88e0736ac2e5989914
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-08-22 14:57:30 -0700

    Almost at mvp just have a few errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,36 @@
 import React, { useState } from "react";
-import { Route } from "react-router-dom";
+import { Route, Link, Switch, withRouter } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
+import axios from 'axios';
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 
-const App = () => {
-  const [savedList, setSavedList] = useState([]);
-
-  const addToSavedList = movie => {
-    setSavedList([...savedList, movie]);
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      savedList: []
+    }
   };
 
-  return (
-    <>
-      <SavedList list={savedList} />
-      <Route exact path="/" component={MovieList} />
-      <Route
-        path="/movies/:id"
+  componentDidmount() {}
+  addToSavedList = movie => {
+    console.log(this.state.savedList);
+    const savedList = this.state.savedList;
+    savedList.push(movie);
+    this.setState({ savedList })
+  };
+  
+  render() {
+    return (
+      <div>
+        <SavedList list={this.state.savedList} />
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id"
         render={props => {
-          return <Movie {...props} addToSavedList={addToSavedList} />;
-        }}
-      />
-    </>
-  );
-};
-
-export default App;
+          return <Movie {...props} addToSavedList={this.addToSavedList} />;
+        }} />
+      </div>
+    );
+  }
+}
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,48 +1,76 @@
 import React from "react";
 import axios from "axios";
 import MovieCard from "./MovieCard";
-export default class Movie extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movie: null
-    };
-  }
+import { Link } from 'react-router-dom';
 
-  componentDidMount() {
-    this.fetchMovie(this.props.match.params.id);
-  }
+
+export default class Movie extends React.Component {
+
+  //   this.state = {
+  //     movie: null
+  //   };
+  // }
+
+  // componentDidMount() {
+  //   this.fetchMovie(this.props.match.params.id);
+  // }
 
   componentWillReceiveProps(newProps) {
+    console.log('newProps', newProps);
     if (this.props.match.params.id !== newProps.match.params.id) {
       this.fetchMovie(newProps.match.params.id);
     }
-  }
-
+  
   fetchMovie = id => {
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
-      .then(res => this.setState({ movie: res.data }))
-      .catch(err => console.log(err.response));
+      .then(response => {
+        this.setState({ movie: response.data });
+      })
+      .catch(err => {
+        console.log(err);
+    });
   };
+
 
   saveMovie = () => {
     const addToSavedList = this.props.addToSavedList;
     addToSavedList(this.state.movie);
   };
 
-  render() {
+  deleteItem = e => {
+    e.preventDefault();
+    axios
+      .delete(`http://localhost:5000/api/movies/${item.id}`)
+      .then(res => {
+        props.updateItems(res.data);
+        props.history.push('/item-list');
+      })
+      .catch(err => console.log(err.response));
+  };
+
+
+  render() 
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
-
     return (
       <div className="save-wrapper">
         <MovieCard movie={this.state.movie} />
+        
         <div className="save-button" onClick={this.saveMovie}>
           Save
         </div>
+        <Link to={"/"}>
+  <div className="home-button-card">Home</div>
+</Link>
       </div>
     );
   }
 }
+
+
+
+
+
+
